856f9a70c8f7b343c11e9832aea03f64b514bbd2
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-06-19 23:48:16 +0900

    change dir tree

---
+++ etc/init/fish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+fisher install 0rax/fish-bd jethrokuan/z oh-my-fish/theme-clearance
--- etc/init/homebrew.sh
+++ etc/init/homebrew.sh
@@ -2,4 +2,4 @@
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-printf "If you want to install brew packages listed in the Brewfile, do `brew bundle`."
+printf "If you want to install brew packages listed in the Brewfile, do `brew bundle --global`."
