5933d7f2f8c9b6b4bcb5a44bf0f106080e1da1cf
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-07-22 22:25:06 +0900

    Add web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2275,6 +2275,20 @@
   :custom-face
   ((vhl/default-face . '((nil (:foreground "#FF3333" :background "#FFCDCD"))))))
 
+(leaf web-mode
+  :ensure t
+  :custom ((web-mode-markup-indent-offset . 2)
+           (web-mode-css-indent-offset . 2)
+           (web-mode-code-indent-offset . 2))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+
 (leaf wgrep
   :doc "Writable grep buffer and apply the changes to files"
   :tag "extensions" "edit" "grep"
