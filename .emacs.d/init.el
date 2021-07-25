95cdd23aadccfb6af6351dc73e727ad503871c91
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-07-25 17:02:23 +0900

    Add ob-async

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1328,35 +1328,11 @@
     (mac-toggle-input-method nil)
     (run-hooks 'my:ime-off-hook))
 
+  (leaf ob-async :ensure t)
+  
   (leaf org-fragtog
     :ensure t
     :hook (org-mode-hook . org-fragtog-mode))
-  
-  (leaf *terminal-notifier
-    :when window-system
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ;; Terminal notifier
-    ;; requires 'brew install terminal-notifier'
-    ;; stolen from erc-notifier
-    :preface
-    (defvar terminal-notifier-command (executable-find "terminal-notifier") "The path to terminal-notifier.")
-
-    ;; (terminal-notifier-notify "Emacs notification" "Something amusing happened")
-
-    (defun terminal-notifier-notify (title message)
-      "Show a message with
-        terminal-notifier-command
-    ."
-      (start-process "terminal-notifier"
-                     "terminal-notifier"
-                     terminal-notifier-command
-                     "-title" title
-                     "-message" message
-                     "-activate" "org.gnu.Emacs"))
-
-    (defun timed-notification (time msg)
-      (interactive "sNotification when (e.g: 2 minutes, 60 seconds, 3 days): \nsMessage: ")
-      (run-at-time time nil (lambda (msg) (terminal-notifier-notify "Emacs" msg)) msg)))
 
 
   (defun jethro/org-archive-done-tasks ()
