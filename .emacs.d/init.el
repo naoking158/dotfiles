1eeee32eb2781c0326b90e1d66c9aec6ca4d6c56
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-08-30 02:53:18 +0900

    Add mu4e

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2553,4 +2553,188 @@ While the dabbrev-abbrev-skip-leading-regexp is instructed to also expand words 
    (python-mode-hook . dap-ui-mode)
    (python-mode-hook . dap-tooltip-mode)))
 
+(leaf mu4e
+  :when window-system
+  :load-path "/usr/local/Cellar/mu/1.6.5/share/emacs/site-lisp/mu/mu4e/"
+  :require t
+  :config
+  (setq mail-user-agent 'mu4e-user-agent
+        message-send-mail-function 'smtpmail-send-it
+        ;; Make sure plain text mails flow correctly for recipients
+        mu4e-compose-format-flowed t
+        ;; This is set to 't' to avoid mail syncing issues when using mbsync
+        mu4e-change-filenames-when-moving t
+        ;; Refresh mail using isync every 10 minutes
+        mu4e-update-interval (* 10 60)
+        mu4e-get-mail-command "mbsync -a"
+        mu4e-maildir "~/Mail")
+  (setq mu4e-contexts
+        (list
+         ;; Work account
+         (make-mu4e-context
+          :name "Work"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/BBO" (mu4e-message-field msg :maildir))))
+          :vars '((user-mail-address			. "naoki@bbo.cs.tsukuba.ac.jp")
+                  (user-full-name					. "naoki@bbo.cs.tsukuba.ac.jp")
+                  (smtpmail-smtp-server		. "smtp.gmail.com")
+                  (smtpmail-smtp-service	. 465)
+                  (smtpmail-stream-type		. 'ssl)
+                  (mu4e-drafts-folder			. "/BBO/[Gmail]/Drafts")
+                  (mu4e-sent-folder				. "/BBO/[Gmail]/Sent Mail")
+                  (mu4e-refile-folder			. "/BBO/[Gmail]/All Mail")
+                  (mu4e-trash-folder			. "/BBO/[Gmail]/Trash")))
+
+         ;; Private account
+         (make-mu4e-context
+          :name "Private"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/Private" (mu4e-message-field msg :maildir))))
+          :vars '((user-mail-address			. "nok.skmt.snow@gmail.com")
+                  (user-full-name					. "nok.skmt.snow@gmail.com")
+                  (smtpmail-smtp-server		. "smtp.gmail.com")
+                  (smtpmail-smtp-service	. 465)
+                  (smtpmail-stream-type		. 'ssl)
+                  (mu4e-drafts-folder			. "/Private/[Gmail]/Drafts")
+                  (mu4e-sent-folder				. "/Private/[Gmail]/Sent Mail")
+                  (mu4e-refile-folder			. "/Private/[Gmail]/All Mail")
+                  (mu4e-trash-folder			. "/Private/[Gmail]/Trash")))))
+
+  (setq mu4e-maildir-shortcuts
+        '((:maildir "/BBO/[Gmail]/All Mail"		:key ?a)
+          (:maildir "/BBO/[Gmail]/Inbox"			:key ?i)
+          (:maildir "/BBO/[Gmail]/Drafts"			:key ?d)
+          (:maildir "/BBO/[Gmail]/Sent Mail"	:key ?s)
+          (:maildir "/BBO/[Gmail]/Trash"			:key ?t)
+          (:maildir "/Private/[Gmail]/Inbox"	:key ?p)
+          ))
+
+  :preface
+  (defun my/org-capture-mu4e ()
+    (interactive)
+    "Capture a TODO item via email."
+    (org-capture nil "o"))
+
+  (add-to-list 'org-capture-templates
+               `("o" "TODO respond to email" entry 
+                 (file ,(concat jethro/org-agenda-directory "inbox.org"))
+                 "* TODO %^{Description}\n%A\n%?\n"))
+
+  :bind
+  (mu4e-headers-mode-map
+   ("{" . mu4e-headers-query-prev)      ; differs from built-in
+   ("}" . mu4e-headers-query-next)      ; differs from built-in
+   ("o" . my/org-capture-mu4e)          ; differs from built-in
+
+   ("A" . mu4e-headers-mark-for-action) ; differs from built-in
+
+   ("`" . mu4e-update-mail-and-index)   ; differs from built-in
+   ("|" . mu4e-view-pipe)               ; does not seem to be built in any longer
+   ("." . hydra-mu4e-headers/body))
+
+  :hydra
+  (hydra-mu4e-headers
+   (:color blue :hint nil)
+   "
+ ^General^   | ^Search^           | _!_: read    | _#_: deferred  | ^Switches^
+-^^----------+-^^-----------------| _?_: unread  | _%_: pattern   |-^^------------------
+_n_: next    | _s_: search        | _r_: refile  | _&_: custom    | _O_: sorting
+_p_: prev    | _S_: edit prev qry | _u_: unmk    | _+_: flag      | _P_: threading
+_]_: n unred | _/_: narrow search | _U_: unmk *  | _-_: unflag    | _Q_: full-search
+_[_: p unred | _b_: search bkmk   | _d_: trash   | _T_: thr       | _V_: skip dups 
+_y_: sw view | _B_: edit bkmk     | _D_: delete  | _t_: subthr    | _W_: include-related
+_R_: reply   | _{_: previous qry  | _m_: move    |-^^-------------+-^^------------------ 
+_C_: compose | _}_: next query    | _a_: action  | _|_: thru shl  | _`_: update, reindex
+_F_: forward | _C-+_: show more   | _A_: mk4actn | _H_: help      | _;_: context-switch
+_o_: org-cap | _C--_: show less   | _*_: *thing  | _q_: quit hdrs | _j_: jump2maildir "
+
+   ;; general
+   ("n" mu4e-headers-next)
+   ("p" mu4e-headers-previous)
+   ("[" mu4e-select-next-unread)
+   ("]" mu4e-select-previous-unread)
+   ("y" mu4e-select-other-view)
+   ("R" mu4e-compose-reply)
+   ("C" mu4e-compose-new)
+   ("F" mu4e-compose-forward)
+   ("o" my/org-capture-mu4e)                  ; differs from built-in
+
+   ;; search
+   ("s" mu4e-headers-search)
+   ("S" mu4e-headers-search-edit)
+   ("/" mu4e-headers-search-narrow)
+   ("b" mu4e-headers-search-bookmark)
+   ("B" mu4e-headers-search-bookmark-edit)
+   ("{" mu4e-headers-query-prev)              ; differs from built-in
+   ("}" mu4e-headers-query-next)              ; differs from built-in
+   ("C-+" mu4e-headers-split-view-grow)
+   ("C--" mu4e-headers-split-view-shrink)
+
+   ;; mark stuff 
+   ("!" mu4e-headers-mark-for-read)
+   ("?" mu4e-headers-mark-for-unread)
+   ("r" mu4e-headers-mark-for-refile)
+   ("u" mu4e-headers-mark-for-unmark)
+   ("U" mu4e-mark-unmark-all)
+   ("d" mu4e-headers-mark-for-trash)
+   ("D" mu4e-headers-mark-for-delete)
+   ("m" mu4e-headers-mark-for-move)
+   ("a" mu4e-headers-action)                  ; not really a mark per-se
+   ("A" mu4e-headers-mark-for-action)         ; differs from built-in
+   ("*" mu4e-headers-mark-for-something)
+
+   ("#" mu4e-mark-resolve-deferred-marks)
+   ("%" mu4e-headers-mark-pattern)
+   ("&" mu4e-headers-mark-custom)
+   ("+" mu4e-headers-mark-for-flag)
+   ("-" mu4e-headers-mark-for-unflag)
+   ("t" mu4e-headers-mark-subthread)
+   ("T" mu4e-headers-mark-thread)
+
+   ;; miscellany
+   ("q" mu4e~headers-quit-buffer)
+   ("H" mu4e-display-manual)
+   ("|" mu4e-view-pipe)                       ; does not seem built-in any longer
+
+   ;; switches
+   ("O" mu4e-headers-change-sorting)
+   ("P" mu4e-headers-toggle-threading)
+   ("Q" mu4e-headers-toggle-full-search)
+   ("V" mu4e-headers-toggle-skip-duplicates)
+   ("W" mu4e-headers-toggle-include-related)
+
+   ;; more miscellany
+   ("`" mu4e-update-mail-and-index)           ; differs from built-in
+   (";" mu4e-context-switch)  
+   ("j" mu4e~headers-jump-to-maildir)
+
+   ("." nil)))
+
+
+(leaf mu4e-views
+  :disabled t
+  :ensure t
+  :after mu4e
+  :bind (mu4e-headers-mode-map
+         :package mu4e
+         ("v" . mu4e-views-mu4e-select-view-msg-method) ;; select viewing method
+         ("M-n" . mu4e-views-cursor-msg-view-window-down) ;; from headers window scroll the email view
+         ("M-p" . mu4e-views-cursor-msg-view-window-up) ;; from headers window scroll the email view
+         ("f" . mu4e-views-toggle-auto-view-selected-message) ;; toggle opening messages automatically when moving in the headers view
+         ("i" . mu4e-views-mu4e-view-as-nonblocked-html) ;; show currently selected email with all remote content
+         )
+  :config
+  (setq mu4e-views-default-view-method "dispatcher") ;; make xwidgets default
+  (mu4e-views-mu4e-use-view-msg-method "dispatcher") ;; select the default
+  (setq mu4e-views-next-previous-message-behaviour 'stick-to-current-window) ;; when pressing n and p stay in the current window
+  (setq mu4e-views-auto-view-selected-message t)  ;; automatically open messages when moving in the headers view
+  (setq mu4e-views-dispatcher-predicate-view-map
+        `((,(lambda (msg) (mu4e-message-field msg :body-html)) . "html")
+          (,(lambda (msg) (ignore msg) t) . "text"))) 
+  )
+
 (provide 'init)
