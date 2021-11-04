19a636005e38a7f5ac94e92e76e289a73db639b9
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-11-04 18:18:10 +0900

    update fish config

--- .config/fish/config-macos.fish
+++ .config/fish/config-macos.fish
@@ -3,11 +3,11 @@ eval (/opt/homebrew/bin/brew shellenv)
 # Homebrew
 set -xg HOMEBREW_EDITOR "/usr/local/bin/emacs -q -nw"
 
-set PATH /Library/Tex/texbin $PATH
+# set PATH /Library/Tex/texbin $PATH
 set PATH /usr/local/bin $PATH
-set PATH /usr/local/texlive/2020basic/bin/x86_64-darwin $PATH
+# set PATH /usr/local/texlive/2020basic/bin/x86_64-darwin $PATH
 set PATH $HOME/.local/bin $PATH
-set PATH /opt/local/bin $PATH
+# set PATH /opt/local/bin $PATH
 
 # Node.js
 set PATH $HOME/.nodebrew/current/bin $PATH
@@ -26,13 +26,23 @@ function brave
     '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser' $argv    
 end
 
-set -gx LDFLAGS "-L/usr/local/opt/ruby/lib" $LDFLAGS
-set -gx CPPFLAGS "-I/usr/local/opt/ruby/include" $CPPFLAGS
-set -gx PKG_CONFIG_PATH "/usr/local/opt/ruby/lib/pkgconfig" $PKG_CONFIG_PATH
-set -gx PKG_CONFIG_PATH "/usr/local/lib/pkgconfig" $PKG_CONFIG_PATH
-set -gx PKG_CONFIG_PATH "/usr/local/lib/pkgconfig" $PKG_CONFIG_PATH
+# set -gx LDFLAGS "-L/usr/local/opt/ruby/lib" $LDFLAGS
+# set -gx CPPFLAGS "-I/usr/local/opt/ruby/include" $CPPFLAGS
+# set -gx PKG_CONFIG_PATH "/usr/local/opt/ruby/lib/pkgconfig" $PKG_CONFIG_PATH
+set -gx LDFLAGS "-L/opt/homebrew/opt/ruby/lib" $LDFLAGS
+set -gx CPPFLAGS "-I/opt/homebrew/opt/ruby/include" $CPPFLAGS
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/ruby/lib/pkgconfig" $PKG_CONFIG_PATH
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/cairo/lib/pkgconfig" $PKG_CONFIG_PATH
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/gtk+3/lib/pkgconfig" $PKG_CONFIG_PATH
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/pkgconfig" $PKG_CONFIG_PATH
+# set -gx PKG_CONFIG_PATH "/usr/local/lib/pkgconfig" $PKG_CONFIG_PATH
+# set -gx PKG_CONFIG_PATH "/usr/local/lib/pkgconfig" $PKG_CONFIG_PATH
 # set -gx LIBRARY_PATH (brew --prefix libgccjit)/lib/gcc/11 $LIBRARY_PATH
 
+set -gx LDFLAGS "-L/opt/homebrew/opt/imagemagick@6/lib" $LDFLAGS
+set -gx CPPFLAGS "-I/opt/homebrew/opt/imagemagick@6/include" $CPPFLAGS
+
+fish_add_path /opt/homebrew/bin
 
 alias reformatpdftoeps='find . -name "*.pdf" -print0 | xargs -0 -I "{}" pdftops -f 1 -l 1 -eps "{}" "{}.eps"'
 alias renamepdfeps='find . -name "*.pdf.eps" -print0 | xargs -0 -I "{}" rename "s/(.*\\.)pdf.eps/$1eps/" "{}"'
