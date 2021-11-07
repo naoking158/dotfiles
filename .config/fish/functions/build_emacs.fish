9610ba6c240731753719c580cd4a9293f4b5df5e
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-11-07 10:42:10 +0900

    fish func: Update the build script of emacs

--- .config/fish/functions/build_emacs.fish
+++ .config/fish/functions/build_emacs.fish
@@ -3,26 +3,25 @@ function build_emacs
         ./autogen.sh
     end
 
-    for patch_file in ~/src/emacs_patches/*
+    for patch_file in ~/.emacs.d/emacs_patches/*
+        echo "Apply patch: $patch_file ..."
         patch -p1 < $patch_file
     end
-
-    export PKG_CONFIG_PATH=/opt/homebrew/opt/pkgconfig:/opt/homebrew/opt/gtk+3/lib/pkgconfig:/opt/homebrew/opt/cairo/lib/pkgconfig:/opt/homebrew/opt/ruby/lib/pkgconfig
-    export CPPFLAGS=-I/opt/homebrew/opt/ruby/include:-I/opt/homebrew/opt/imagemagick@6/include
-    export LDFLAGS=-L/opt/homebrew/opt/ruby/lib:-L/opt/homebrew/opt/imagemagick@6/lib
 
     ./configure \
         --with-ns \
         --with-modules \
         --with-xwidgets \
         --with-native-compilation \
-        --with-cairo \
+        CPPFLAGS=-I/opt/homebrew/opt/ruby/include \
+        LDFLAGS=-L/opt/homebrew/opt/ruby/lib
         
-    make -j(nproc)
-    make install
-    cp src/emacs nextstep/Emacs.app/Contents/MacOS/bin/
+    gmake -j(nproc) bootstrap
+    gmake install
 
     if test -e ~/src/github.com/emacsfodder/emacs-icons-project
+        echo ""
+        echo "Change Emacs icons ..."
         cd ~/src/github.com/emacsfodder/emacs-icons-project/
         cp EmacsIcon3.icns \
             ~/src/github.com/emacs-mirror/emacs/nextstep/Emacs.app/Contents/Resources/Emacs.icons
@@ -32,5 +31,9 @@ function build_emacs
             ~/src/github.com/emacs-mirror/emacs/nextstep/Emacs.app/Contents/Info.plist.bak
         cp document-icons/Info.plist \
             ~/src/github.com/emacs-mirror/emacs/nextstep/Emacs.app/Contents/Info.plist
+        cd -
     end
+
+    echo ""
+    echo "Build is Done!"
 end
