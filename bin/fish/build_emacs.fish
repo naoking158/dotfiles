7c6fb5ef0d4ef44a7ca107bb83b1ed399c382d8e
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-12-07 09:20:13 +0900

    fish: Change location of fish functions

--- bin/fish/build_emacs.fish
+++ bin/fish/build_emacs.fish
@@ -50,31 +50,9 @@ function build_emacs --argument-names 'emacs_version'
     end
 
     gmake install
-
-    echo '#!/usr/bin/env bash
-
-resolve_link() {
-  "$(type -p greadlink readlink | head -1)" "$1"
-}
-
-abs_dirname() {
-  local path="$1"
-  local name
-  local cwd
-  cwd="$(pwd)"
-
-  while [ -n "$path" ]; do
-    cd "${path%/*}" || exit 1
-    name="${path##*/}"
-    path="$(resolve_link "$name" || true)"
-  done
-
-  pwd
-  cd "$cwd" || exit 1
-}
-
-exec "$(dirname "$(abs_dirname "$0")")/Emacs" "$@"' > "./nextstep/Emacs.app/Contents/MacOS/bin/emacs"
-    
+    cp ~/.dotfiles/etc/helper/emacs-cli.bash ~/src/github.com/emacs-mirror/emacs/nextstep/Emacs.app/Contents/MacOS/bin/emacs
+    chmod +w ~/src/github.com/emacs-mirror/emacs/nextstep/Emacs.app/Contents/MacOS/bin/emacs
+ 
     echo \n"Build is Done!
 Finally, move `./nextstep/Emacs.app` into `/Applications/` and do bellow commands:
 
@@ -82,9 +60,4 @@ Finally, move `./nextstep/Emacs.app` into `/Applications/` and do bellow command
     sudo ln -s /Applications/Emacs.app/Contents/MacOS/bin/emacsclient /usr/local/bin/emacsclient
 
     "
-end
-
-
-function cleate_emacs_cli_tool
-    
 end
