4010d48fd22d62864601a2a009b6d96e72eb793e
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2021-12-24 15:39:02 +0900

    emacs: Show meow-status in modeline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -676,14 +676,14 @@
     :when window-system
     :ensure t
     :config
-    (setq my--modeline-gui-rw-symbol " 📖"
-          my--modeline-tty-rw-symbol " RW"
+    (setq my--modeline-gui-rw-symbol "📖"
+          my--modeline-tty-rw-symbol "RW"
 
-          my--modeline-gui-ro-symbol " 📙"
-          my--modeline-tty-ro-symbol " RO"
+          my--modeline-gui-ro-symbol "📙"
+          my--modeline-tty-ro-symbol "RO"
           
-          my--modeline-gui-mod-symbol " ✏️"
-          my--modeline-tty-mod-symbol " **")
+          my--modeline-gui-mod-symbol "✏️"
+          my--modeline-tty-mod-symbol "**")
     
     (defun my--modeline-status ()
       "Return buffer status: default symbols are read-only (📙)/(RO),
@@ -709,13 +709,8 @@ modified (✏️)/(**), or read-write (📖)/(RW)"
       (moody-replace-mode-line-buffer-identification)
       (moody-replace-vc-mode)
       (moody-replace-eldoc-minibuffer-message-function)
-      (moody-replace-element 'mode-line-frame-identification '(" ")
-                             ;; '(:eval
-                             ;;   (propertize
-                             ;;    (concat
-                             ;;     " " (alist-get 'name (tab-bar--current-tab)) " ")
-                             ;;    'face '(:weight bold)))
-                             )
+      (moody-replace-element 'mode-line-frame-identification
+                             '(:eval (meow-indicator)))
       (moody-replace-element 'mode-line-mule-info '(""))
       (moody-replace-element 'mode-line-client '(""))
       (moody-replace-element 'mode-line-remote '(""))
