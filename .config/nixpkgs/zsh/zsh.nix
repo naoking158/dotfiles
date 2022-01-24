09da539cea324cc5f25305a37e89328ce83d1e1b
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2022-01-24 09:52:19 +0900

    zsh: Add conda activate

--- .config/nixpkgs/zsh/zsh.nix
+++ .config/nixpkgs/zsh/zsh.nix
@@ -79,6 +79,21 @@
     if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
     fi
+
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$(\"$MINICONDA/bin/conda\" 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "$MINICONDA/etc/profile.d/conda.sh" ]; then
+            . "$MINICONDA/etc/profile.d/conda.sh"
+        else
+            export PATH="$MINICONDA/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
     '';
   
   initExtraBeforeCompInit = ''
