7c54d40d4064e03e5b1209dfa8fce9f9d3e20ec9
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2022-02-17 23:07:50 +0900

    nix: Add setup file for manjaro

---
+++ .config/nixpkgs/manjaro.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./common.nix ];
+
+  home = {
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+    sessionVariables = {
+      EDITOR = "vim";
+    };
+
+    # Home Manager needs a bit of information about you and the
+    # paths it should manage.
+    username = "naoki";
+    homeDirectory = "/home/naoki";
+    
+    packages = with pkgs; [
+      ntp
+      bluez      
+    ];
+  };
+
+  i18n.inputMethod.enabled = "fcitx5";
+  services.fluidsynth.soundService = "pipewire-pulse";
+}
