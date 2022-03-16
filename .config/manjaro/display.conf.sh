ae9379868633ba1482c4a597381ad90082d6dd4d
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2022-03-16 12:03:50 +0900

    update

---
+++ .config/manjaro/display.conf.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-A-0 --mode 3440x1440 --pos 200x0 --rotate normal --output HDMI-A-1 --off --output DisplayPort-0 --primary --mode 3840x1600 --pos 0x1440 --rotate normal --output DisplayPort-1 --off
