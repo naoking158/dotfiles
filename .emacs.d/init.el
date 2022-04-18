59c3b2b79ea72126cd57ca10f2d47c78fc96bb15
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2022-04-18 23:13:44 +0900

    emacs: Add mermaid-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3622,4 +3622,24 @@ Interactively, URL defaults to the string looking like a url around point."
     (add-to-list 'markdown-preview-javascript elm))
   )
 
+(leaf mermaid-mode
+  :ensure t
+  :defer-config
+  (setq mermaid-mode-map
+        (let ((map mermaid-mode-map))
+          (define-key map (kbd "C-c C-c") nil)
+          (define-key map (kbd "C-c C-f") nil)
+          (define-key map (kbd "C-c C-b") nil)
+          (define-key map (kbd "C-c C-r") nil)
+          (define-key map (kbd "C-c C-o") nil)
+          (define-key map (kbd "C-c C-d") nil)
+          (define-key map (kbd "C-c C-d c") 'mermaid-compile)
+          (define-key map (kbd "C-c C-d c") 'mermaid-compile)
+          (define-key map (kbd "C-c C-d f") 'mermaid-compile-file)
+          (define-key map (kbd "C-c C-d b") 'mermaid-compile-buffer)
+          (define-key map (kbd "C-c C-d r") 'mermaid-compile-region)
+          (define-key map (kbd "C-c C-d o") 'mermaid-open-browser)
+          (define-key map (kbd "C-c C-d d") 'mermaid-open-doc)
+          map)))
+
 (provide 'init)
