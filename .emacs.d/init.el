3746eaaea2b578537971d4a89eae08b0f74c9b6d
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2022-06-05 18:50:12 +0900

    emacs: Update notmuch-archive-tags

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3170,7 +3170,7 @@ While the dabbrev-abbrev-skip-leading-regexp is instructed to also expand words 
   (setq notmuch-show-empty-saved-searches t)
 
 ;;; Tags
-  (setq notmuch-archive-tags '("-inbox" "+archived"))
+  (setq notmuch-archive-tags '("-inbox" "-del" "-todo" "+archived"))
   (setq notmuch-message-replied-tags '("+replied"))
   (setq notmuch-message-forwarded-tags '("+forwarded"))
   (setq notmuch-show-mark-read-tags '("-unread"))
