71ae78c33f1e10c4ce1c5ce357109b45c0bd1ec9
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2023-05-09 19:40:00 +0900

    emacs: Delete indent-region-custom

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1457,51 +1457,6 @@ modified (✏️)/(**), or read-write (📖)/(RW)"
                 (highlight-indent-guides-even-face . '((nil (:foreground "#88ca9f"))))
                 (highlight-indent-guides-character-face . '((nil (:foreground "#2266ae"))))))
 
-(leaf *indent-region-custom
-  :doc "This should be used in GUI Emacs to avoid inserting weired characters in CUI Emacs."
-  :when window-system
-  :preface
-  (defun indent-region-custom(numSpaces)
-    (progn
-      ;; default to start and end of current line
-      (setq regionStart (line-beginning-position))
-      (setq regionEnd (line-end-position))
-      ;; if there's a selection, use that instead of the current line
-      (when (use-region-p)
-        (setq regionStart (region-beginning))
-        (setq regionEnd (region-end))
-        )
-
-      (save-excursion ; restore the position afterwards
-        (goto-char regionStart) ; go to the start of region
-        (setq start (line-beginning-position)) ; save the start of the line
-        (goto-char regionEnd) ; go to the end of region
-        (setq end (line-end-position)) ; save the end of the line
-
-        (indent-rigidly start end numSpaces) ; indent between start and end
-        (setq deactivate-mark nil) ; restore the selected region
-        )))
-  :config
-  (leaf *untab-region
-    :bind (("C-M-[" . untab-region))
-    :preface
-    (defun untab-region nil
-      (interactive)
-      (indent-region-custom -4)))
-
-  (leaf *tab-region
-    :bind ("C-M-]" . tab-region)
-    :preface
-    (defun tab-region nil
-      (interactive)
-      (if (active-minibuffer-window)
-          (minibuffer-complete)    ; tab is pressed in minibuffer window -> do completion
-        (if (use-region-p)    ; tab is pressed is any other buffer -> execute with space insertion
-            (indent-region-custom 4) ; region was selected, call indent-region-custom
-          (insert "    ") ; else insert four spaces as expected
-          ))
-      )))
-
 (leaf tree-sitter
   :straight t tree-sitter-langs
   :hook (tree-sitter-after-on-hook . tree-sitter-hl-mode)
