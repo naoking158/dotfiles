2add1fee5ba28cf9ae87e4b49f87d64cfd64e904
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2023-05-28 00:30:28 +0900

    update setup-vpn.sh

--- bin/setup-vpn.sh
+++ bin/setup-vpn.sh
@@ -13,11 +13,13 @@ case $1 in
         sleep 5
         dhcpcd vpn_vpn-fp
         ip route add 10.0.0.0/8 via 10.224.192.1 dev vpn_vpn-fp proto static metric 200 onlink
+        ip route add 20.48.61.83 via 10.224.192.1 dev vpn_vpn-fp proto static metric 200 onlink
         ip route del default via 10.224.192.1 dev vpn_vpn-fp proto dhcp src 10.224.192.11
         ;;
     "down")
         vpncmd localhost /CLIENT /CMD Accountdisconnect sakamoto
         ip route del 10.0.0.0/8 via 10.224.192.1 dev vpn_vpn-fp proto static metric 200 onlink
+        ip route del 20.48.61.83 via 10.224.192.1 dev vpn_vpn-fp proto static metric 200 onlink
         nmcli connect down vpn_vpn-fp
         systemctl stop softethervpn-client
         systemctl restart NetworkManager
