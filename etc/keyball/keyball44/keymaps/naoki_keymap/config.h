8fa3bcc3f3fe7cf432349b73a5326af577740de8
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2023-11-02 15:36:12 +0900

    update keymap

--- etc/keyball/keyball44/keymaps/naoki_keymap/config.h
+++ etc/keyball/keyball44/keymaps/naoki_keymap/config.h
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
+//#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
 #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
@@ -35,5 +35,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 170
 #define QUICK_TAP_TERM_PER_KEY
