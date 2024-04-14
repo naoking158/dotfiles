85d281600a14c97277d82ba7ec50373a33787d3b
naoking158 <naoki@bbo.cs.tsukuba.ac.jp>
2024-04-14 18:48:08 +0900

    update

--- dot_local/etc/keyball/keyball44/keymaps/naoki_keymap/keymap.c
+++ dot_local/etc/keyball/keyball44/keymaps/naoki_keymap/keymap.c
@@ -24,24 +24,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-                         LGUI_T(KC_TAB) , KC_Q    , KC_W         , KC_E         , KC_R       , KC_T       ,   KC_Y          , KC_U           , KC_I    , KC_O     , KC_P          , KC_MINUS ,
-                         KC_LCTL        , KC_A    , KC_S         , KC_D         , KC_F       , LT(3,KC_G) ,   KC_H          , KC_J           , KC_K    , KC_L     , KC_SCLN       , KC_QUOTE ,
-                         KC_LSFT        , KC_Z    , KC_X         , KC_C         , KC_V       , KC_B       ,   KC_N          , KC_M           , KC_COMM , KC_DOT   , LT(3,KC_SLSH) , KC_BTN1  ,
-                            LALT_T(KC_ESC)        , KC_LGUI , LT(2,KC_SPC) , LT(1,KC_SPC) , LALT(KC_X) ,                LT(1,KC_BSPC) , LT(2,KC_ENT) , _______ ,  _______ , KC_RALT
+                         LGUI_T(KC_TAB) , KC_Q  , KC_W    , KC_E           , KC_R           , KC_T               ,   KC_Y , KC_U           , KC_I         , KC_O    , KC_P          , KC_MINUS ,
+                         KC_LCTL        , KC_A  , KC_S    , KC_D           , KC_F           , LT(3,KC_G)         ,   KC_H , KC_J           , KC_K         , KC_L    , KC_SCLN       , KC_QUOTE ,
+                         LALT(KC_X)     , KC_Z  , KC_X    , KC_C           , KC_V           , KC_B               ,   KC_N , KC_M           , KC_COMM      , KC_DOT  , LT(3,KC_SLSH) , KC_BTN1  ,
+                                LALT_T(KC_ESC)  , KC_LGUI , LSFT_T(KC_SPC) , LGUI_T(KC_SPC) , LT(1, KC_BTN2)     ,           LT(2,KC_BSPC) , LT(1,KC_ENT) , _______ , _______       , KC_RALT
                          ),
 
   [1] = LAYOUT_universal(
-                         LGUI_T(KC_GRAVE), KC_EXCLAIM , KC_AT   , KC_HASH , KC_DOLLAR , KC_PERCENT        ,   KC_CIRCUMFLEX, KC_AMPERSAND  , KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN , KC_EQUAL         ,
-                         KC_LCTL         , _______    , KC_BTN2 , KC_UP   , KC_BTN1   , KC_RGUI           ,   KC_LEFT      , KC_DOWN       , KC_UP      , KC_RGHT      , KC_LEFT_BRACKET, KC_RIGHT_BRACKET ,
-                         KC_LSFT         , _______    , KC_LEFT , KC_DOWN , KC_RIGHT  , RGUI(KC_RALT)     ,   _______      , _______       , _______    , _______      , KC_BACKSLASH   , _______          ,
-                               LALT_T(KC_ESC)         , KC_LGUI    , _______ , _______ , _______   ,                       KC_BSPC      , RGUI_T(KC_ENT), _______    , _______      , KC_RALT
+                         LGUI_T(KC_GRAVE), KC_EXCLAIM , KC_AT   , KC_HASH        , KC_DOLLAR      , KC_PERCENT         ,   KC_CIRCUMFLEX, KC_AMPERSAND  , KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN , KC_EQUAL         ,
+                         KC_LCTL         , _______    , KC_BTN2 , KC_UP          , KC_BTN1        , KC_RGUI            ,   KC_LEFT      , KC_DOWN       , KC_UP      , KC_RGHT      , KC_LEFT_BRACKET, KC_RIGHT_BRACKET ,
+                         KC_LSFT         , _______    , KC_LEFT , KC_DOWN        , KC_RIGHT       , RGUI(KC_RALT)      ,   _______      , _______       , _______    , KC_BTN2      , KC_BACKSLASH   , KC_BTN1          ,
+                                      LALT_T(KC_ESC)  , KC_LGUI , LSFT_T(KC_SPC) , LGUI_T(KC_SPC) , LT(1, KC_BTN2)     ,                    LT(2,KC_BSPC) , LT(1,KC_ENT) , _______    , _______      , KC_RALT
                          ),
 
   [2] = LAYOUT_universal(
                          KC_TILDE       , _______ , _______ , _______ , _______ , _______        ,      _______   , KC_7, KC_8    , KC_9    , _______            , KC_PLUS              ,
                          KC_LCTL        , _______ , _______ , _______ , KC_RGUI , RSG(KC_RCTL)   ,      KC_DOT   , KC_4, KC_5    , KC_6    , KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE ,
                          KC_LSFT        , _______ , _______ , _______ , _______ , _______        ,      KC_0     , KC_1, KC_2    , KC_3    , KC_PIPE            , _______              ,
-                            LALT_T(KC_ESC)        , KC_LGUI , _______ , _______ , _______ ,                       KC_BSPC   , _______, _______ , _______ , _______
+                                LALT_T(KC_ESC)    , KC_LGUI , LSFT_T(KC_SPC) , LGUI_T(KC_SPC) ,  LT(1, KC_BTN2) ,                       KC_BSPC   , _______, _______ , _______ , _______
                          ),
 
   [3] = LAYOUT_universal(
